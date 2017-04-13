41451e30d3332be59b705df23a59ef49ee8f4799
Echqo <adam.tokarczyk@student.put.poznan.pl>
2017-04-13 08:17:07 +0200

    Tag entity and TagDao added

---
+++ src/main/java/VandRC/models/Tag.java
@@ -0,0 +1,49 @@
+package VandRC.models;
+
+import javax.persistence.*;
+
+/**
+ * Created by Adam on 13.04.2017.
+ */
+@Entity
+@Table(name = "users")
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int tagID;
+    private int toolID;
+    private String tagName;
+
+    public Tag() {
+    }
+
+    public Tag(int toolID, String tagName) {
+        this.toolID = toolID;
+        this.tagName = tagName;
+    }
+
+    public int getTagID() {
+        return tagID;
+    }
+
+    public void setTagID(int tagID) {
+        this.tagID = tagID;
+    }
+
+    public int getToolID() {
+        return toolID;
+    }
+
+    public void setToolID(int toolID) {
+        this.toolID = toolID;
+    }
+
+    public String getTagName() {
+        return tagName;
+    }
+
+    public void setTagName(String tagName) {
+        this.tagName = tagName;
+    }
+}
---
+++ src/main/java/VandRC/models/TagDao.java
@@ -0,0 +1,23 @@
+package VandRC.models;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+/**
+ * Created by Adam on 13.04.2017.
+ */
+@Repository
+@Transactional
+public class TagDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Tag> getAll() {
+        return entityManager.createQuery("from Tag").getResultList();
+    }
+}
